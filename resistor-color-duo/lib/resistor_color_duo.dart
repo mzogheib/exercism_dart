c4f411f5291ebed351df2d5f3a01f5a9852480af
Marwan Zogheib <mzogheib@gmail.com>
2021-08-07 10:35:34 +1000

    Pass: resistor-color-duo

--- resistor-color-duo/lib/resistor_color_duo.dart
+++ resistor-color-duo/lib/resistor_color_duo.dart
@@ -1,3 +1,25 @@
 class ResistorColorDuo {
-  // Put your code here
+  static const _colors = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white',
+  ];
+
+  int _getColorCode(String color) {
+    return _colors.indexOf(color);
+  }
+
+  int value(List<String> colors) {
+    final stringValue =
+        colors.take(2).map(_getColorCode).map((v) => v.toString()).join('');
+
+    return int.parse(stringValue);
+  }
 }
