165946ce699fb075b2f350c3c9b2356c36e3e03e
Marwan Zogheib <mzogheib@gmail.com>
2021-08-07 18:20:31 +1000

    Pass tests for leap

--- leap/lib/leap.dart
+++ leap/lib/leap.dart
@@ -1,3 +1,26 @@
+// on every year that is evenly divisible by 4
+// except every year that is evenly divisible by 100
+// unless the year is also evenly divisible by 400
+
 class Leap {
-  // Put your code here
+  bool _isDivisibleBy(int num, int divisor) {
+    return num % divisor == 0;
+  }
+
+  bool _isDivisibleBy4(int num) {
+    return _isDivisibleBy(num, 4);
+  }
+
+  bool _isDivisibleBy100(int num) {
+    return _isDivisibleBy(num, 100);
+  }
+
+  bool _isDivisibleBy400(int num) {
+    return _isDivisibleBy(num, 400);
+  }
+
+  bool leapYear(int year) {
+    return _isDivisibleBy4(year) && !_isDivisibleBy100(year) ||
+        (_isDivisibleBy4(year) && _isDivisibleBy400(year));
+  }
 }
