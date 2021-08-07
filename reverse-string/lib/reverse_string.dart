4cc811c5ae812757a26ea3a1c861a6d75f672313
Marwan Zogheib <mzogheib@gmail.com>
2021-08-07 10:53:39 +1000

    Pass: reverse-string

--- reverse-string/lib/reverse_string.dart
+++ reverse-string/lib/reverse_string.dart
@@ -1,3 +1,13 @@
-String reverse() {
-  // Put your code here
+String reverse(String input) {
+  final chars = input.split('');
+  final length = chars.length;
+
+  List<String> outputChars = [];
+
+  for (int i = 0; i < length; i++) {
+    final oppositeChar = chars[length - i - 1];
+    outputChars.add(oppositeChar);
+  }
+
+  return outputChars.join('');
 }
